d85d5c349196a8fd8e8516aa661eff63cd5d7e5b
RashmikaE <rashmikapramodya@gmail.com>
2023-12-04 09:06:05 +0530

    changes made

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,21 +3,32 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target; // Reference to the car's transform
-    public Vector3 offset = new Vector3(8, 54, -84); // Camera offset from the car
-    public float smoothTime = 0.3f; // Smoothing time for camera movement
+    public float distance = 47f; // Distance from the car
+    public float height = 26f; // Height above the car
+    public float rotationDamping = 2f; // Damping for camera rotation
 
-    private Vector3 velocity = Vector3.zero; // Velocity for SmoothDamp
-
-    void Update()
+    void LateUpdate()
     {
         if (target == null)
             return;
 
-        // Calculate the desired camera position
-        Vector3 desiredPosition = target.position + offset;
-        // Use SmoothDamp to smoothly move the camera towards the desired position
-        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
-        // Look at the car
-        transform.LookAt(target);
+        // Calculate the desired rotation angle based on the car's rotation
+        float desiredRotationAngle = target.eulerAngles.y;
+        float desiredHeight = target.position.y + height;
+
+        // Calculate the current rotation angle and height using Mathf.Lerp for smoothing
+        float currentRotationAngle = Mathf.LerpAngle(transform.eulerAngles.y, desiredRotationAngle, rotationDamping * Time.deltaTime);
+        float currentHeight = Mathf.Lerp(transform.position.y, desiredHeight, rotationDamping * Time.deltaTime);
+
+        // Convert the angle into a rotation
+        Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+        // Set the position of the camera behind the car based on the desired distance and height
+        Vector3 newPosition = target.position - currentRotation * Vector3.forward * distance;
+        newPosition.y = currentHeight;
+
+        // Set the position and rotation of the camera
+        transform.position = newPosition;
+        transform.LookAt(target.position);
     }
 }
